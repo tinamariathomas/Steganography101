e5309b03f18971a97cd3d3f277ff98fd652918ab
tinamaria <tinamariathomas@gmail.com>
2015-11-02 15:26:00 +0530

    Added methods to manipulate image pixels

--- src/com/company/Image.java
+++ src/com/company/Image.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,11 +11,12 @@ public class Image {
 
     int pixelMatrix[][];
     private int length, width;
+    BufferedImage image;
 
     Image(String filePath) throws IOException {
 
         File file = new File(filePath);// file object to get the file, the second argument is the name of the image file
-        BufferedImage image = ImageIO.read(file);
+        image = ImageIO.read(file);
         Raster image_raster = image.getData();
 
         //get pixel by pixel
@@ -40,5 +42,34 @@ public class Image {
 
     public int getWidth() {
         return width;
+    }
+
+    public int getPixelAt(int x, int y) {
+        return pixelMatrix[x][y];
+    }
+
+    public void setPixelAt(int x, int y, int newPixelValue) {
+        pixelMatrix[x][y] = newPixelValue;
+    }
+
+    public void writeAsImageToFile(String filePath) throws IOException {
+
+        int pixelMatrixFlattened[] = new int[getLength() * getWidth()];
+
+        int k = 0;
+
+        for (int i = 0; i < getWidth(); i++) {
+            for (int j = 0; j < getLength(); j++) {
+                pixelMatrixFlattened[k++] = pixelMatrix[j][i];
+            }
+        }
+
+        BufferedImage pixelImage = new BufferedImage(length, width, BufferedImage.TYPE_BYTE_GRAY);
+
+        WritableRaster raster = (WritableRaster) image.getData();
+        raster.setPixels(0, 0, length, width, pixelMatrixFlattened);
+        pixelImage.setData(raster);
+
+        ImageIO.write(pixelImage, "jpg", new File(filePath));
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Image image = new Image("/Users/tinamt/Desktop/819679cf77816357ef7c056c606891d1.jpg");
+        Image image = new Image("/Users/tinamt/Desktop/puppy1.jpg");
         System.out.println(image.getLength());
         System.out.println(image.getWidth());
 
-        
+        for (int i = 0; i < image.getLength(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                image.setPixelAt(i, j, image.getPixelAt(i, j));
+            }
+        }
+        image.writeAsImageToFile("/Users/tinamt/Desktop/output4.jpg");
     }
 }
